48a73b8edde27ca2c983c6dee04c6b243da586ac
PepitoRigolo <romain.ricard.dev@gmail.com>
2022-08-08 14:22:56 +0200

    adding dice

--- app/src/main/java/fr/pepitorigolo/rolldice/MainActivity.kt
+++ app/src/main/java/fr/pepitorigolo/rolldice/MainActivity.kt
@@ -15,19 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDices() }
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollDices() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
+        val secondDice = Dice(6)
         val diceRoll = dice.roll()
+        val secondDiceRoll = secondDice.roll()
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
+        val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView.text = diceRoll.toString()
+        resultTextView2.text = secondDiceRoll.toString()
     }
 }
 
